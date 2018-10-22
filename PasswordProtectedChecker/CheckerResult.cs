447ed48e54a248b66053a7b81c6cf066629a6cc7
sicos1977 <sicos2002@hotmail.com>
2018-10-22 18:10:51 +0200

    Added CheckerResult class

---
+++ PasswordProtectedChecker/CheckerResult.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace PasswordProtectedChecker
+{
+    /// <summary>
+    /// A placeholder to return the result of a check with a breadcrum trail about the
+    /// file that has a password when the result is <c>true</c>
+    /// </summary>
+    public class CheckerResult
+    {
+        #region Properties
+        /// <summary>
+        /// Returns the result
+        /// </summary>
+        public bool Result { get; }
+
+        /// <summary>
+        /// Returns the breadcrumb trail to the file that returns <c>true</c> in the
+        /// <see cref="Result"/> property
+        /// </summary>
+        public List<string> BreadCrumbs => new List<string>();
+        #endregion
+
+        #region AddBreadCrumbTrail
+        public void AddBreadCrumbTrail(string trail)
+        {
+            BreadCrumbs.Add(trail);
+        }
+        #endregion
+    }
+}
