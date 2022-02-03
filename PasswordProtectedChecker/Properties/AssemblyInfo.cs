f92f38701f29266b05c922bbb6cdcb6838397db1
Kees van Spelde <keesvanspelde@hotmail.com>
2022-02-03 18:28:32 +0100

    Updates

--- PasswordProtectedChecker/Properties/AssemblyInfo.cs
+++ PasswordProtectedChecker/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PasswordProtectedChecker")]
-[assembly: AssemblyCopyright("Copyright © 2018 - 2020 Kees van Spelde")]
+[assembly: AssemblyCopyright("Copyright © 2018 - 2022 Kees van Spelde")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.5.0")]
-[assembly: AssemblyFileVersion("1.3.5.0")]
+[assembly: AssemblyVersion("1.3.8.0")]
+[assembly: AssemblyFileVersion("1.3.8.0")]
